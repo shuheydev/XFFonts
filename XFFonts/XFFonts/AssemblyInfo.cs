9341c688428c0f739bc5296c85bb5a2f08b9d03b
shuheydev <shuhey.dev@gmail.com>
2020-03-11 17:24:58 +0900

    Add project files.

---
+++ XFFonts/XFFonts/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Pacifico-Regular.ttf")]
+[assembly: ExportFont("material.ttf")]
+[assembly: ExportFont("Raleway-Black.ttf")]
+[assembly: ExportFont("Raleway-Regular.ttf")]
---
+++ XFFonts/XFFonts/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFFonts
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var label = new Label
+            {
+                Text = "Welcome to Xamarin.Forms! From C#",
+                FontFamily = Device.RuntimePlatform == Device.Android ? "Bellota-Regular.ttf#Bellota" : null
+            };
+            label.HorizontalOptions = LayoutOptions.Center;          
+            stackLayout.Children.Add(label);
+        }
+    }
+}
